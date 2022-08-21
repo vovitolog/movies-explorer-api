a89c620961ed64abdc7a68e56a6caf2debcda6ae
vovitolog <vovitolog@gmail.com>
2022-08-21 10:51:30 +0300

    feat: добавить модель user

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
