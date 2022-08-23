71ae258ee274f077b8089c822a20f55bf0a8e32c
vovitolog <vovitolog@gmail.com>
2022-08-23 22:36:40 +0300

    feat: добавить helmet

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const cors = require('./middlewares/cors-protection');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./middlewares/rate-limiter');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
   useNewUrlParser: true,
