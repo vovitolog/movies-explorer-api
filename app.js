5dc0901a304f4e028d78b66b20f8301e71a8152e
vovitolog <vovitolog@gmail.com>
2022-08-23 22:14:25 +0300

    feat: добавить логирование

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 const cors = require('./middlewares/cors-protection');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./middlewares/rate-limiter');
 
 const { PORT = 3000 } = process.env;
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
 app.use(router);
 app.use(rateLimiter);
 app.use(cors);
+app.use(requestLogger);
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
