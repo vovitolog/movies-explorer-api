d32543c042b4031b209d21acc74d3c5f7aeedfce
vovitolog <vovitolog@gmail.com>
2022-08-26 08:27:44 +0300

    feat: добавить константы и config.js

---
+++ utils/config.js
@@ -0,0 +1,8 @@
+const {
+  DATABASE = 'mongodb://127.0.0.1:27017/moviesdb',
+  PORT = 3000,
+} = process.env;
+
+module.exports = {
+  DATABASE, PORT,
+};
---
+++ utils/constants.js
@@ -0,0 +1,25 @@
+const userNotFoundMsg = 'Пользователь не найден';
+const badRequestMsg = 'Введены некорректные данные';
+const userExistsMsg = 'Данный емейл уже зарегистрирован';
+const wrongUrlMsg = 'Введите корректный URL';
+const wrongEmailMsg = 'Неправильный формат почты';
+const authErrorMsg = 'Необходима авторизация';
+const serverErrorMsg = 'На сервере произошла ошибка';
+const moviesNotFoundMsg = 'Фильмы не найдены';
+const movieNotFoundMsg = 'Фильм не найден';
+const deleteMovieForbiddenMsg = 'Нет прав для удаления фильма';
+const deleteMovieSuccessfulMsg = 'Фильм удален';
+
+module.exports = {
+  userNotFoundMsg,
+  badRequestMsg,
+  userExistsMsg,
+  wrongEmailMsg,
+  wrongUrlMsg,
+  authErrorMsg,
+  serverErrorMsg,
+  moviesNotFoundMsg,
+  movieNotFoundMsg,
+  deleteMovieForbiddenMsg,
+  deleteMovieSuccessfulMsg,
+};
