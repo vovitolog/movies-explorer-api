49837495d099efeb1f5410dd580985391b5b3e94
vovitolog <vovitolog@gmail.com>
2022-09-25 15:50:44 +0300

    fix: поправить cors

--- middlewares/cors-protection.js
+++ middlewares/cors-protection.js
@@ -5,6 +5,8 @@ const allowedDomains = [
   'http://api.movies.vovitolog.nomoredomains.sbs',
   'http://localhost:3001',
   'https://localhost:3001',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
 ];
 
 module.exports = (req, res, next) => {
