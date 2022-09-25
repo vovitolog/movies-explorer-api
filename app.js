21a2a0e13302db5effda67182c8a0b965d5b9a48
vovitolog <vovitolog@gmail.com>
2022-09-25 17:03:21 +0300

    fix: поправить cors

--- app.js
+++ app.js
@@ -17,14 +17,13 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors);
 mongoose.connect(DATABASE, {
   useNewUrlParser: true,
 });
 app.use(requestLogger);
 app.use(rateLimiter);
 app.use(router);
-app.use(cors);
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
