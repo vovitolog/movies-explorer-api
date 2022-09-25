09049026637c6190107e84dcf8d3873002104da0
vovitolog <vovitolog@gmail.com>
2022-09-25 15:36:08 +0300

    fix: отключить cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const error = require('./middlewares/error');
-const cors = require('./middlewares/cors-protection');
+/* const cors = require('./middlewares/cors-protection'); */
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./middlewares/rate-limiter');
 const router = require('./routes/index');
@@ -23,7 +23,7 @@ mongoose.connect(DATABASE, {
 app.use(requestLogger);
 app.use(rateLimiter);
 app.use(router);
-app.use(cors);
+/* app.use(cors); */
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
